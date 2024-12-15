973b014053c0272007177190eb9ae88f08749feb
yangdaqiong <ydaqiong@gmai.com>
2024-12-16 01:30:54 +0800

    feat: add picture

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ A collection of UI components for yew.
 - [gloo](https://docs.rs/gloo/latest/gloo/)
 - [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
 
+## Button
+![img](./docs/assets/button.jpg)
+
 ## License
+
 This project is licensed under the MIT License. See the [LICENSE](./LICENSE.txt) file for details.
--- examples/src/main.rs
+++ examples/src/main.rs
@@ -4,19 +4,32 @@ use yew_ui::gloo::console::log;
 
 use yew_ui::toolkit::math::add;
 
-
 #[function_component(App)]
 fn app() -> Html {
     log!(add(1, 2));
     html! {
-        <main class="container">
-        <Button text="Primary 按钮" click={|_| log!("点击了确认按钮")} r#type={TypeStyle::Primary}/>
-        <Button text="Warning 按钮" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-        <Button text="Danger 按钮" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Danger}/>
-        <Button text="Success 按钮" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Success}/>
-        <Button text="Info 按钮" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Info}/>
-        <Button text="默认按钮" click={|_| log!("点击了取消按钮")} />
-        <h1>{"Welcome to Tauri + Yew"}</h1>
+        <main class="p-5">
+        <h1>{"Welcome Yew + YewUI"}</h1>
+        <dl>
+            <dt class="py-2">{"基础按钮"}</dt>
+            <dd>
+                <Button text="Primary 按钮" custom_class="mr-2" click={|_| log!("点击了确认按钮")} r#type={TypeStyle::Primary}/>
+                <Button text="Warning 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
+                <Button text="Danger 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Danger}/>
+                <Button text="Success 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Success}/>
+                <Button text="Info 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Info}/>
+                <Button text="默认按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} />
+            </dd>
+            <dt class="py-2">{"块按钮"}</dt>
+            <dd>
+                <Button text="Primary 按钮" block=true custom_class="mb-2" click={|_| log!("点击了确认按钮")} r#type={TypeStyle::Primary}/>
+                <Button text="Warning 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
+                <Button text="Danger 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Danger}/>
+                <Button text="Success 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Success}/>
+                <Button text="Info 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Info}/>
+                <Button text="默认按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} />
+            </dd>
+        </dl>
     </main>
     }
 }
