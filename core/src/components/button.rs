cc3080d52054dfe9ab039e81c309a62cef8743c7
yangdaqiong <ydaqiong@gmai.com>
2024-12-16 23:11:28 +0800

    Completed disabled link

--- core/src/components/button.rs
+++ core/src/components/button.rs
@@ -9,6 +9,7 @@ pub struct Props {
     #[prop_or_default]
     pub text: Option<String>,
 
+    #[prop_or_default]
     pub click: Callback<()>,
 
     #[prop_or_default]
@@ -25,6 +26,12 @@ pub struct Props {
 
     #[prop_or_default]
     pub block: bool,
+
+    #[prop_or_default]
+    pub link: bool,
+
+    #[prop_or_default]
+    pub disabled: bool,
 }
 
 #[function_component(Button)]
@@ -34,15 +41,41 @@ pub fn button(props: &Props) -> Html {
         click_handler.emit(());
     });
 
-    let mut type_style = match props.r#type {
-        TypeStyle::Primary => "bg-blue-500 hover:bg-blue-600 hover:border-blue-700 text-white font-bold py-2 px-4".to_string(),
-        TypeStyle::Warning => "bg-orange-500 hover:bg-orange-600 hover:border-orange-700 text-white font-bold py-2 px-4".to_string(),
-        TypeStyle::Danger => "bg-red-500 hover:bg-red-600 hover:border-red-700 text-white font-bold py-2 px-4".to_string(),
-        TypeStyle::Success => "bg-green-500 hover:bg-green-600 hover:border-green-700 text-white font-bold py-2 px-4".to_string(),
-        TypeStyle::Info => "bg-sky-400 hover:bg-sky-500 hover:border-sky-700 text-white font-bold py-2 px-4".to_string(),
-        TypeStyle::Default => "bg-gray-100 hover:bg-gray-200 hover:border-gray-300 font-bold py-2 px-4".to_string(),
+    let mut type_style = if props.link {
+        match props.r#type {
+            TypeStyle::Primary => "text-blue-600 hover:text-blue-300 py-2 px-4".to_string(),
+            TypeStyle::Warning => "text-orange-500 hover:text-orange-300 py-2 px-4".to_string(),
+            TypeStyle::Danger => "text-red-600 hover:text-red-300 py-2 px-4".to_string(),
+            TypeStyle::Success => "text-green-600 hover:text-green-300 py-2 px-4".to_string(),
+            TypeStyle::Info => "text-sky-600 hover:text-sky-300 py-2 px-4".to_string(),
+            TypeStyle::Default => "text-gray-600 hover:text-gray-300 py-2 px-4".to_string(),
+        }
+    } else {
+        match props.r#type {
+            TypeStyle::Primary => {
+                "bg-blue-500 hover:bg-blue-600 hover:border-blue-700 text-white py-2 px-4"
+                    .to_string()
+            }
+            TypeStyle::Warning => {
+                "bg-orange-500 hover:bg-orange-600 hover:border-orange-700 text-white py-2 px-4"
+                    .to_string()
+            }
+            TypeStyle::Danger => {
+                "bg-red-500 hover:bg-red-600 hover:border-red-700 text-white py-2 px-4".to_string()
+            }
+            TypeStyle::Success => {
+                "bg-green-500 hover:bg-green-600 hover:border-green-700 text-white py-2 px-4"
+                    .to_string()
+            }
+            TypeStyle::Info => {
+                "bg-sky-400 hover:bg-sky-500 hover:border-sky-700 text-white py-2 px-4".to_string()
+            }
+            TypeStyle::Default => {
+                "bg-gray-100 hover:bg-gray-200 hover:border-gray-300 py-2 px-4".to_string()
+            }
+        }
     };
-    
+
     let custom_class = props.custom_class.clone().unwrap_or("".to_string());
     if !custom_class.is_empty() {
         type_style = format!("{} {}", type_style, custom_class);
@@ -55,74 +88,79 @@ pub fn button(props: &Props) -> Html {
     match props.size {
         SizeStyle::Xl9 => {
             type_style = format!("{} text-9xl", type_style);
-        },
+        }
         SizeStyle::Xl8 => {
             type_style = format!("{} text-8xl", type_style);
-        },
+        }
         SizeStyle::Xl7 => {
             type_style = format!("{} text-7xl", type_style);
-        },
+        }
         SizeStyle::Xl6 => {
             type_style = format!("{} text-6xl", type_style);
-        },
+        }
         SizeStyle::Xl5 => {
             type_style = format!("{} text-5xl", type_style);
-        },
+        }
         SizeStyle::Xl4 => {
             type_style = format!("{} text-4xl", type_style);
-        },
+        }
         SizeStyle::Xl3 => {
             type_style = format!("{} text-3xl", type_style);
-        },
+        }
         SizeStyle::Xl2 => {
             type_style = format!("{} text-2xl", type_style);
-        },
+        }
         SizeStyle::Xl => {
             type_style = format!("{} text-xl", type_style);
-        },
+        }
         SizeStyle::Lg => {
             type_style = format!("{} text-lg", type_style);
-        },
+        }
         SizeStyle::Base => {
             type_style = format!("{} text-base", type_style);
-        },
+        }
         SizeStyle::Sm => {
             type_style = format!("{} text-sm", type_style);
-        },
+        }
         SizeStyle::Xs => {
             type_style = format!("{} text-xs", type_style);
         }
-        _ => type_style = format!("{} text-base", type_style)
+        _ => type_style = format!("{} text-base", type_style),
     };
 
     match props.rounded {
         Rounded::Full => {
             type_style = format!("{} rounded-full", type_style);
-        },
+        }
         Rounded::Xl => {
             type_style = format!("{} rounded-xl", type_style);
-        },
+        }
         Rounded::Xl2 => {
             type_style = format!("{} rounded-2xl", type_style);
-        },
+        }
         Rounded::Xl3 => {
             type_style = format!("{} rounded-3xl", type_style);
-        },
+        }
         Rounded::Lg => {
             type_style = format!("{} rounded-lg", type_style);
-        },
+        }
         Rounded::Rounded => {
             type_style = format!("{} rounded-md", type_style);
-        },
+        }
         Rounded::Sm => {
             type_style = format!("{} rounded-sm", type_style);
-        },
+        }
         Rounded::None => {
             type_style = format!("{} rounded-none", type_style);
-        },
-        _ => type_style = format!("{} rounded-none", type_style)
+        }
+        _ => type_style = format!("{} rounded-none", type_style),
     }
+
+    if props.disabled {
+        type_style = format!("{} opacity-50 cursor-not-allowed", type_style);
+    }
+
     html! {
-        <button onclick={click} class={type_style}>{ format!("{}", props.text.clone().unwrap_or("按钮".to_string())) }</button>
+        <button onclick={click} disabled={props.disabled} class={type_style}>{ format!("{}", props.text.clone().unwrap_or("按钮".to_string())) }</button>
     }
 }
--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use yew_ui::components::prelude::*;
 use yew_ui::gloo::console::log;
+use yew_ui::gloo::dialogs::alert;
 
 use yew_ui::toolkit::math::add;
 
@@ -9,53 +10,89 @@ fn app() -> Html {
     log!(add(1, 2));
     html! {
         <main class="p-5 text-4xl">
-        <h1>{"Welcome Yew + YewUI"}</h1>
+        <h1>{"YewUI = Yew + Tailwindcss"}</h1>
         <dl>
-            <dt class="pt-4 text-2xl">{"基础按钮"}</dt>
+            <dt class="pt-4 text-2xl">{"Basic usage"}</dt>
             <dd class="pb-6">
-                <Button text="Primary 按钮" custom_class="mr-2" click={|_| log!("点击了确认按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Warning 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Danger 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Danger}/>
-                <Button text="Success 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Success}/>
-                <Button text="Info 按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Info}/>
-                <Button text="默认按钮" custom_class="mr-2" click={|_| log!("点击了取消按钮")} />
+                <Button text="Primary" custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Warning" custom_class="mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Danger" custom_class="mr-2" r#type={TypeStyle::Danger}/>
+                <Button text="Success" custom_class="mr-2" r#type={TypeStyle::Success}/>
+                <Button text="Info" custom_class="mr-2"  r#type={TypeStyle::Info}/>
+                <Button text="Default" custom_class="mr-2" />
             </dd>
-            <dt class="py-4 text-2xl">{"圆角按钮"}</dt>
+            <dt class="pt-4 text-2xl">{"Disabled Button"}</dt>
             <dd class="pb-6">
-                <Button text="Full" rounded={Rounded::Full} custom_class="mb-2 mr-2" click={|_| log!("点击了确认按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Xl3" rounded={Rounded::Xl3} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Xl2" rounded={Rounded::Xl2} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Xl" rounded={Rounded::Xl} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Lg"  rounded={Rounded::Lg} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Rounded"  rounded={Rounded::Rounded} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Sm"  rounded={Rounded::Sm} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="None"  rounded={Rounded::None} custom_class="mb-2 mr-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
+                <Button text="Primary" disabled={true}  custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Warning" disabled={true} custom_class="mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Danger" disabled={true} custom_class="mr-2" r#type={TypeStyle::Danger}/>
+                <Button text="Success" disabled={true} custom_class="mr-2" r#type={TypeStyle::Success}/>
+                <Button text="Info" disabled={true} custom_class="mr-2"  r#type={TypeStyle::Info}/>
+                <Button text="Default" disabled={true} custom_class="mr-2" />
             </dd>
-            <dt class="py-4 text-2xl">{"块按钮"}</dt>
+            <dt class="pt-4 text-2xl">{"Basic link button"}</dt>
             <dd class="pb-6">
-                <Button text="Primary 按钮" block=true custom_class="mb-2" click={|_| log!("点击了确认按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Warning 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Warning}/>
-                <Button text="Danger 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Danger}/>
-                <Button text="Success 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Success}/>
-                <Button text="Info 按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} r#type={TypeStyle::Info}/>
-                <Button text="默认按钮" block=true custom_class="mb-2" click={|_| log!("点击了取消按钮")} />
+                <Button text="Primary" link={true} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Warning" link={true} custom_class="mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Danger" link={true} custom_class="mr-2" r#type={TypeStyle::Danger}/>
+                <Button text="Success" link={true} custom_class="mr-2" r#type={TypeStyle::Success}/>
+                <Button text="Info" link={true} custom_class="mr-2"  r#type={TypeStyle::Info}/>
+                <Button text="Default" link={true} custom_class="mr-2" />
             </dd>
-            <dt class="py-4 text-2xl">{"按钮尺寸"}</dt>
+            <dt class="pt-4 text-2xl">{"Disabled link button"}</dt>
             <dd class="pb-6">
-                <Button text="Xs" size={SizeStyle::Xs} custom_class="mr-2" click={|_| log!("点击了 Xs 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Sm" size={SizeStyle::Sm} custom_class="mr-2" click={|_| log!("点击了 Sm 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Default" custom_class="mr-2" click={|_| log!("点击了 Xs 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Base" size={SizeStyle::Base} custom_class="mr-2" click={|_| log!("点击了 Lg 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Lg" size={SizeStyle::Lg} custom_class="mr-2" click={|_| log!("点击了 Lg 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl" size={SizeStyle::Xl} custom_class="mr-2" click={|_| log!("点击了 Xl 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl2" size={SizeStyle::Xl2} custom_class="mr-2" click={|_| log!("点击了 Xl2 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl3" size={SizeStyle::Xl3} custom_class="mr-2" click={|_| log!("点击了 Xl3 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl4" size={SizeStyle::Xl4} custom_class="mr-2" click={|_| log!("点击了 Xl4 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl5" size={SizeStyle::Xl5} custom_class="mr-2" click={|_| log!("点击了 Xl5 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl6" size={SizeStyle::Xl6} custom_class="mr-2" click={|_| log!("点击了 Xl6 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl7" size={SizeStyle::Xl7} custom_class="mr-2" click={|_| log!("点击了 Xl7 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl8" size={SizeStyle::Xl8} custom_class="mr-2" click={|_| log!("点击了 Xl8 按钮")} r#type={TypeStyle::Primary}/>
-                <Button text="Xl9" size={SizeStyle::Xl9} custom_class="mr-2" click={|_| log!("点击了 Xl9 按钮")} r#type={TypeStyle::Primary}/>
+                <Button text="Primary" disabled={true} link={true} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Warning" disabled={true} link={true} custom_class="mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Danger" disabled={true} link={true} custom_class="mr-2" r#type={TypeStyle::Danger}/>
+                <Button text="Success" disabled={true} link={true} custom_class="mr-2" r#type={TypeStyle::Success}/>
+                <Button text="Info" disabled={true} link={true} custom_class="mr-2"  r#type={TypeStyle::Info}/>
+                <Button text="Default" disabled={true} link={true} custom_class="mr-2" />
+            </dd>
+            <dt class="py-4 text-2xl">{"Rounded"}</dt>
+            <dd class="pb-6">
+                <Button text="Full" rounded={Rounded::Full} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Xl3" rounded={Rounded::Xl3} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Xl2" rounded={Rounded::Xl2} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Xl" rounded={Rounded::Xl} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Lg"  rounded={Rounded::Lg} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Rounded"  rounded={Rounded::Rounded} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="Sm"  rounded={Rounded::Sm} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+                <Button text="None"  rounded={Rounded::None} custom_class="mb-2 mr-2" r#type={TypeStyle::Warning}/>
+            </dd>
+            <dt class="py-4 text-2xl">{"Icon Button"}</dt>
+            <dd class="pb-6">
+                { "developing ... "}
+            </dd>
+            <dt class="py-4 text-2xl">{"Block"}</dt>
+            <dd class="pb-6 w-[700px]">
+                <Button text="Primary" block=true custom_class="mb-2" r#type={TypeStyle::Primary}/>
+                <Button text="Warning" block=true custom_class="mb-2" r#type={TypeStyle::Warning}/>
+                <Button text="Danger" block=true custom_class="mb-2" r#type={TypeStyle::Danger}/>
+                <Button text="Success" block=true custom_class="mb-2" r#type={TypeStyle::Success}/>
+                <Button text="Info" block=true custom_class="mb-2" r#type={TypeStyle::Info}/>
+                <Button text="Default" block=true custom_class="mb-2" />
+            </dd>
+            <dt class="py-4 text-2xl">{"Sizes"}</dt>
+            <dd class="pb-6">
+                <Button text="Xs" size={SizeStyle::Xs} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Sm" size={SizeStyle::Sm} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Default" custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Base" size={SizeStyle::Base} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Lg" size={SizeStyle::Lg} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                <Button text="Xl" size={SizeStyle::Xl} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl2" size={SizeStyle::Xl2} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl3" size={SizeStyle::Xl3} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl4" size={SizeStyle::Xl4} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl5" size={SizeStyle::Xl5} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl6" size={SizeStyle::Xl6} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl7" size={SizeStyle::Xl7} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl8" size={SizeStyle::Xl8} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+                // <Button text="Xl9" size={SizeStyle::Xl9} custom_class="mr-2" r#type={TypeStyle::Primary}/>
+            </dd>
+            <dt class="py-4 text-2xl">{"Event"}</dt>
+            <dd class="pb-6">
+                <Button text="console" rounded={Rounded::Full} custom_class="mb-2 mr-2" click={|_| log!("The button has been clicked")} r#type={TypeStyle::Success}/>
+                <Button text="alert" rounded={Rounded::Full} custom_class="mb-2" click={|_| alert("The button has been clicked")} r#type={TypeStyle::Success}/>
             </dd>
         </dl>
     </main>
