e4581c6980983232322ee6cc88c12ad4502d0f6d
yangdaqiong <ydaqiong@gmai.com>
2024-12-25 20:52:03 +0800

    feat: Complete the chain manipulation encapsulation of border styles

--- examples/src/pages/border.rs
+++ examples/src/pages/border.rs
@@ -1,5 +1,4 @@
 use yew::prelude::*;
-use yew_ui::components::prelude::*;
 
 #[function_component(PageBorder)]
 pub fn page_border() -> Html {
@@ -7,13 +6,13 @@ pub fn page_border() -> Html {
         <div class="container">
             <h1 class="text-2xl">{"Border"}</h1>
             <p>{"我们对边框进行统一规范，可用于按钮、卡片、弹窗等组件里。"}</p>
-            <Container border={Border{
-                width: SetBorderWidth::default(), 
-                color: BorderColor::Red(ColorEnum::V500, BorderPosition::Default),
-                style: BorderStyle::Dashed
-            }.to_string()}>
-                {1111}
-            </Container>
+            // <Container border={Border{
+            //     width: SetBorderWidth::default(), 
+            //     color: BorderColor::Red(ColorEnum::V500, BorderPosition::Default),
+            //     style: BorderStyle::Dashed
+            // }.to_string()}>
+            //     {1111}
+            // </Container>
         </div>
     };
 }
